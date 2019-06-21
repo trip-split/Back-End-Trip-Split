2ed918d2e811dbe1b5fec01364480321541d76c4
Will Schulz <will.schulz29@gmail.com>
2019-06-21 02:09:04 -0400

    post user route because I deleted the auth one

--- users/users-router.js
+++ users/users-router.js
@@ -11,5 +11,16 @@ router.get('/', (req, res) => {
     .catch(err => res.send(err));
 });
 
+router.post('/new-user', (req, res) => {
+  let user = req.body;
+  Users.insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({error: 'error registering user'});
+    });
+});
+
 
 module.exports = router;
